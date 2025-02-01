47d8296eae084d71a7608635bc2d15dc298d8382
Jakob Kampichler <me@jkdev.io>
2025-02-01 18:10:56 +0100

    data_catalogue: delete functionality

---
+++ src/app/api/data_catalogue/delete/route.ts
@@ -0,0 +1,26 @@
+import { getSession } from "@/lib/session";
+import { NextRequest, NextResponse } from "next/server";
+import { client } from "@/lib/sharedEEGClient";
+
+export async function DELETE(req: NextRequest) {
+    let session = await getSession();
+
+    if(!session.is_verified)
+        return new NextResponse("", { status: 403 });
+
+    let body = await req.json();
+    if(!Object.keys(body).includes("object_id") || body["object_id"] === undefined)
+        { return new NextResponse("bad req", { status: 400 }); }
+
+    let res = await client.delete_catalogue_entry(body["object_id"], session.user?.bPK!!);
+    
+    let code = res["status_code"];
+    delete res["status_code"];
+
+    return new NextResponse(JSON.stringify({ res }), {
+        headers: {
+            'content-type': "application/json"
+        },
+        status: code
+    });
+}
--- src/app/data/requests.ts
+++ src/app/data/requests.ts
@@ -1,3 +1,15 @@
+import * as config from "@/lib/config";
+
+export async function fetchServices() {
+    let res = await fetch(config.SERVICES_URL);
+    let json = await res.json();
+
+    if(res.status !== 200) 
+        { throw Error(JSON.stringify(json)); }
+    
+    return json;
+}
+
 export async function fetchObjects(page: number) {
     let res = await fetch(`/api/data_catalogue/get_eeg_objects?page=${page}`);
     let json = await res.json();
@@ -18,6 +30,15 @@ export async function fetchObject(object_id: string) {
     return json;
 }
 
+export async function deleteObject(object_id: string) {
+    let res = await fetch("/api/data_catalogue/delete", {
+        method: "DELETE",
+        body: JSON.stringify({ object_id }),
+        headers: { "content-type": "application/json" }
+    });
+    let json = await res.json();
+    return json;
+}
 
 export async function postD2A(data: any) {
     let res = await fetch("/api/data_catalogue/submit", {
